0a71196c6379e5b9b6a0363f7b484e0c40fea902
Tejas Patil <95238373+Tejas2305@users.noreply.github.com>
2024-08-31 20:48:59 +0530

    Update index.php

--- Security/Head/index.php
+++ Security/Head/index.php
@@ -153,10 +153,159 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     $phone,
                                     $nextDateString
                                 );
+
+                                if ($insert_stmt->execute()) {
+                                    $string .= "$c] Data inserted successfully for $selection for shift on $current_day from $current_start_time till $current_end_time at $current_wing Wing dated on $nextDateString.<br><br>";
+                                } else {
+                                    $string .=
+                                        "$c] Error inserting data for $selection for shift on $current_day from $current_start_time till $current_end_time at $current_wing Wing dated on $nextDateString: " .
+                                        htmlspecialchars($insert_stmt->error) .
+                                        "<br><br>";
+                                }
+                                $insert_stmt->close();
                             }
+                            $c++;
                         }
+                    } else {
+                        $string .=
+                            "$c] No matching records found for selection " .
+                            htmlspecialchars($selection) .
+                            ".<br><br>";
+                        $c++;
                     }
+                    $stmt->close();
+                } else {
+                    $string .=
+                        "$c] Error executing query for selection " .
+                        htmlspecialchars($selection) .
+                        ": " .
+                        htmlspecialchars($stmt->error) .
+                        "<br><br>";
+                    $c++;
                 }
             }
         }
-              ?>
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        #nav-icon1,
+        #nav-icon2,
+        #nav-icon3,
+        #nav-icon4 {
+            width: 60px;
+            height: 45px;
+            position: relative;
+            margin: 50px auto;
+            -webkit-transform: rotate(0deg);
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            transform: rotate(0deg);
+            -webkit-transition: .5s ease-in-out;
+            -moz-transition: .5s ease-in-out;
+            -o-transition: .5s ease-in-out;
+            transition: .5s ease-in-out;
+            cursor: pointer;
+        }
+
+        #nav-icon1 span,
+        #nav-icon3 span,
+        #nav-icon4 span {
+            display: block;
+            position: absolute;
+            height: 9px;
+            width: 100%;
+            background: #d3531a;
+            border-radius: 9px;
+            opacity: 1;
+            left: 0;
+            -webkit-transform: rotate(0deg);
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            transform: rotate(0deg);
+            -webkit-transition: .25s ease-in-out;
+            -moz-transition: .25s ease-in-out;
+            -o-transition: .25s ease-in-out;
+            transition: .25s ease-in-out;
+        }
+
+
+        /* Icon 2 */
+
+        #nav-icon2 {
+            position: fixed;
+            left: 10px;
+        }
+
+        #nav-icon2 span {
+            display: block;
+            position: absolute;
+            height: 7px;
+            width: 50%;
+            background: #c7c7c7;
+            opacity: 1;
+            -webkit-transform: rotate(0deg);
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            transform: rotate(0deg);
+            -webkit-transition: .25s ease-in-out;
+            -moz-transition: .25s ease-in-out;
+            -o-transition: .25s ease-in-out;
+            transition: .25s ease-in-out;
+
+        }
+
+        #nav-icon2 span:nth-child(even) {
+            left: 50%;
+            border-radius: 0 9px 9px 0;
+        }
+
+        #nav-icon2 span:nth-child(odd) {
+            left: 0px;
+            border-radius: 9px 0 0 9px;
+        }
+
+        #nav-icon2 span:nth-child(1),
+        #nav-icon2 span:nth-child(2) {
+            top: 0px;
+        }
+
+        #nav-icon2 span:nth-child(3),
+        #nav-icon2 span:nth-child(4) {
+            top: 18px;
+        }
+
+        #nav-icon2 span:nth-child(5),
+        #nav-icon2 span:nth-child(6) {
+            top: 36px;
+        }
+
+        #nav-icon2.open span:nth-child(1),
+        #nav-icon2.open span:nth-child(6) {
+            -webkit-transform: rotate(45deg);
+            -moz-transform: rotate(45deg);
+            -o-transform: rotate(45deg);
+            transform: rotate(45deg);
+        }
+
+        #nav-icon2.open span:nth-child(2),
+        #nav-icon2.open span:nth-child(5) {
+            -webkit-transform: rotate(-45deg);
+            -moz-transform: rotate(-45deg);
+            -o-transform: rotate(-45deg);
+            transform: rotate(-45deg);
+        }
+</style>
+</head>
+</html>
