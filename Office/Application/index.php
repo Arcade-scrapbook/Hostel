f6ed3ab576c808fb0e672a423808a43f90dd867c
Tejas Patil <95238373+Tejas2305@users.noreply.github.com>
2024-08-31 22:40:13 +0530

    Update index.php

--- Office/Application/index.php
+++ Office/Application/index.php
@@ -147,6 +147,157 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             transition: .25s ease-in-out;
         }
 
-</style>
+
+        /* Icon 2 */
+
+        #nav-icon2 {
+            position: fixed;
+            left: 10px;
+        }
+
+        #nav-icon2 span {
+            display: block;
+            position: absolute;
+            height: 7px;
+            width: 50%;
+            background: #c7c7c7;
+            opacity: 1;
+            -webkit-transform: rotate(0deg);
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            transform: rotate(0deg);
+            -webkit-transition: .25s ease-in-out;
+            -moz-transition: .25s ease-in-out;
+            -o-transition: .25s ease-in-out;
+            transition: .25s ease-in-out;
+
+        }
+
+        #nav-icon2 span:nth-child(even) {
+            left: 50%;
+            border-radius: 0 9px 9px 0;
+        }
+
+        #nav-icon2 span:nth-child(odd) {
+            left: 0px;
+            border-radius: 9px 0 0 9px;
+        }
+
+        #nav-icon2 span:nth-child(1),
+        #nav-icon2 span:nth-child(2) {
+            top: 0px;
+        }
+
+        #nav-icon2 span:nth-child(3),
+        #nav-icon2 span:nth-child(4) {
+            top: 18px;
+        }
+
+        #nav-icon2 span:nth-child(5),
+        #nav-icon2 span:nth-child(6) {
+            top: 36px;
+        }
+
+        #nav-icon2.open span:nth-child(1),
+        #nav-icon2.open span:nth-child(6) {
+            -webkit-transform: rotate(45deg);
+            -moz-transform: rotate(45deg);
+            -o-transform: rotate(45deg);
+            transform: rotate(45deg);
+        }
+
+        #nav-icon2.open span:nth-child(2),
+        #nav-icon2.open span:nth-child(5) {
+            -webkit-transform: rotate(-45deg);
+            -moz-transform: rotate(-45deg);
+            -o-transform: rotate(-45deg);
+            transform: rotate(-45deg);
+        }
+
+        #nav-icon2.open span:nth-child(1) {
+            left: 5px;
+            top: 7px;
+        }
+
+        #nav-icon2.open span:nth-child(2) {
+            left: calc(50% - 5px);
+            top: 7px;
+        }
+
+        #nav-icon2.open span:nth-child(3) {
+            left: -50%;
+            opacity: 0;
+        }
+
+        #nav-icon2.open span:nth-child(4) {
+            left: 100%;
+            opacity: 0;
+        }
+
+        #nav-icon2.open span:nth-child(5) {
+            left: 5px;
+            top: 29px;
+        }
+
+        #nav-icon2.open span:nth-child(6) {
+            left: calc(50% - 5px);
+            top: 29px;
+        }
+
+        .top {
+
+            display: flex;
+            flex-direction: row;
+            /* Added this line to correct the syntax */
+        }
+
+        /* Initial state: hidden */
+        .hidden {
+            opacity: 0;
+            transition: opacity 2s ease-out;
+        }
+
+        /* Visible state: shown */
+        .visible {
+            opacity: 1;
+            transition: opacity 2s ease-in;
+
+        }
+    </style>
 </head>
+<body class="wrapper">
+    <div class="main-container">
+        <div class="header">
+            <div class="top">
+                <div id="nav-icon2">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="logo">Gpp Hostel
+                    <span class=logo-det># Boys</span>
+                </div>
+
+            </div>
+            <a class="header-link active" href="/Office">
+                Dashboard
+            </a>
+            <a class="header-link" href="/">
+                Home
+            </a>
+            <a class="header-link" href="About">
+                About us
+            </a>
+            <a class="header-link" href="#">
+                Committee
+            </a>
+            <a class="header-link" href="/Office/Application">
+                Add
+            </a>
+        </div>
+    </div>
+</body>
 </html>
