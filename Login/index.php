49f9569d4765a85d65e23cfbe5d25121376019e5
Tejas Patil <95238373+Tejas2305@users.noreply.github.com>
2024-08-31 17:06:29 +0530

    Update index.php

--- Login/index.php
+++ Login/index.php
@@ -105,4 +105,121 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['wing'] = $wing;
         $_SESSION['room'] = $room;
         $_SESSION['birthdate'] = $birthdate;
+
+        // Reset login attempts on successful login
+        $reset_attempts_query = "DELETE FROM login_attempts WHERE ip_address = ?";
+        $reset_stmt = mysqli_prepare($connection, $reset_attempts_query);
+        mysqli_stmt_bind_param($reset_stmt, "s", $ip_address);
+        mysqli_stmt_execute($reset_stmt);
+
+        switch ($role) {
+            case 'admin':
+                header("Location: ../Admin");
+                exit;
+            case 'student':
+                header("Location: ../Student");
+                exit;
+            case 'office':
+                header("Location: ../Office");
+                exit;
+            case 'security_head':
+                header("Location: ../Security/Head");
+                exit;
+            case 'security':
+                header("Location: ../Security");
+                exit;
+            case 'unblock':
+                header("Location: ../Unblock");
+                exit;
+            default:
+                echo "unknown role"; // Redirect to login page if role is unknown
+                exit;
+        }
+    } else {
+        echo "Invalid username or password";
+
+        // Update login attempts on failed login
+        if ($check_result && mysqli_num_rows($check_result) > 0) {
+            if ($attempt_data['last_attempt_date'] == $date_today) {
+                $remaining_attempts = 4 - $attempt_data['attempt_count'];
+                $update_attempts_query = "UPDATE login_attempts SET attempt_count = attempt_count + 1 WHERE ip_address = ?";
+                $update_stmt = mysqli_prepare($connection, $update_attempts_query);
+                mysqli_stmt_bind_param($update_stmt, "s", $ip_address);
+                mysqli_stmt_execute($update_stmt);
+                echo "<br>Remaining attempts: $remaining_attempts";
+            } else {
+                $reset_attempts_query = "UPDATE login_attempts SET attempt_count = 1, last_attempt_date = ? WHERE ip_address = ?";
+                $reset_stmt = mysqli_prepare($connection, $reset_attempts_query);
+                mysqli_stmt_bind_param($reset_stmt, "ss", $date_today, $ip_address);
+                mysqli_stmt_execute($reset_stmt);
+                echo "<br>Remaining attempts: 4";
+            }
+        } else {
+            $insert_attempts_query = "INSERT INTO login_attempts (ip_address, attempt_count, last_attempt_date) VALUES (?, 1, ?)";
+            $insert_stmt = mysqli_prepare($connection, $insert_attempts_query);
+            mysqli_stmt_bind_param($insert_stmt, "ss", $ip_address, $date_today);
+            mysqli_stmt_execute($insert_stmt);
+            echo "<br>Remaining attempts: 4";
+        }
+    }
+}
 ?>
+
+<!doctype html public "-//w3c//dtd html 3.2//en">
+<html>
+
+<head>
+    <link rel="icon" type="image/png" href="/favicon.png" />
+    <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+    <script src='https://kit.fontawesome.com/a16a49be46.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form class='form rotate' action='' method=post>
+        <!-- Added 'rotate' class here -->
+        <h1>Welcome Back</h1>
+        <p>Enter your credentials to continue.</p>
+
+        <div class='input-wrapper'>
+            <input type='text' name='userid' placeholder='Enter your username' />
+            <i class='fa-solid fa-user'></i>
+        </div>
+
+        <div class='input-wrapper'>
+            <input type='password' placeholder='Enter your password' id='pass' name='password' />
+            <i class='fa-solid fa-lock' onclick='hide()' id='lock'></i>
+        </div>
+
+        <div class='button-wrapper'>
+            <button type='submit'>
+                Sign In&nbsp;
+                <i class='fa-solid fa-arrow-right-to-bracket'></i>
+            </button>
+        </div>
+    </form>
+    <br>
+    <br>
+    <footer>
+        <div>
+            <span>Created By <a id='c' href=''>Tejas Patil</a> | <span class='far fa-copyright' aria-hidden='true'></span> 2024 All rights
+                reserved.</span>
+        </div>
+    </footer>
+
+    <script>
+        function hide() {
+            var x = document.getElementById('pass');
+            var y = document.getElementById('lock');
+            if (x.type === 'password') {
+                x.type = 'text';
+                y.className = 'fa-solid fa-unlock-keyhole';
+            } else {
+                x.type = 'password';
+                y.className = 'fa-solid fa-lock';
+            }
+        }
+    </script>
+</body>
+
+</html>
